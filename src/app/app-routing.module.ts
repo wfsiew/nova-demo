1376253c97a050f1e969420a4de0b615605c14d3
Siew Wing Fei <wingfei.siew@nova-hub.com>
2021-07-30 02:35:55 +0800

    i

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,7 +40,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     scrollPositionRestoration: 'top',
-    useHash: true,
     preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule]
